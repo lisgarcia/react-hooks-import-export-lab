aa1aa632de5e2b0442e67bccbda0fbd67e84033a
Elisabeth Garcia <lisgarciae@gmail.com>
2023-05-23 22:17:29 -0400

    complete

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import About from "./components/About"
+import Home from './components/Home'
+import NavBar from './components/NavBar'
+
 import App from "./components/App";
+
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
